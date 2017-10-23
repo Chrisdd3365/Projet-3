9aca9b67f39f8b8ef575be7c68665718702cb111
Chrisdd3365 <32330136+Chrisdd3365@users.noreply.github.com>
2017-10-23 18:53:57 +0200

    Initial Commit

---
+++ projet3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  projet3
+//
+//  Created by Christophe DURAND on 23/10/2017.
+//  Copyright © 2017 Christophe DURAND. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
